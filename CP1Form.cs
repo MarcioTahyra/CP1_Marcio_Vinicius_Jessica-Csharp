56fd152527039588e797b45ec3db1fe8d75f9a3b
MarcioTahyra <129909992+MarcioTahyra@users.noreply.github.com>
2025-03-27 10:59:40 -0300

    Formulário

---
+++ CP1Form.cs
@@ -0,0 +1,30 @@
+namespace CP1CsharpForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
